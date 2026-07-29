7dc110d13b1006908134081fbc904195047190d3
Byungjin Park <posquit0.bj@gmail.com>
2026-07-29 17:18:42 +0900

    docs(authentication-policy): note unimplemented resource attributes

--- modules/authentication-policy/main.tf
+++ modules/authentication-policy/main.tf
@@ -2,6 +2,9 @@
 # Okta Authentication Policy
 ###################################################
 
+# INFO: Unimplemented attributes
+# - `priority`
+# - `catch_all`
 resource "okta_app_signon_policy" "this" {
   name        = var.name
   description = var.description
